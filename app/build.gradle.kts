725300294565b68ed76c2ea83b92d82a65ac2f87
Adam Rhéaume <adamrheaume@gmail.com>
2026-09-13 21:43:44 -0400

    Add XLSX parser test runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,4 +6,7 @@ android {
  buildTypes { release { isMinifyEnabled = false; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro") } }
  compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
 }
-dependencies { testImplementation("junit:junit:4.13.2") }
+dependencies {
+ testImplementation("junit:junit:4.13.2")
+ testImplementation("xpp3:xpp3:1.1.4c")
+}
