3a71552444f5ba9eddaf28ef996e859cdf1faa85
Adam Rhéaume <adamrheaume@gmail.com>
2026-09-13 21:03:15 -0400

    Bump Culture du Québec to 3.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "quebec.culture.donnees"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "3.1.0"
+        versionCode = 23
+        versionName = "3.2.0"
     }
 
     buildTypes {
