96fecda04e7cecefa006ba8ac5cfa030f0fa794a
Adam Rhéaume <adamrheaume@gmail.com>
2026-09-13 22:40:26 -0400

    Bump Culture du Québec to 3.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins { id("com.android.application") }
 android {
  namespace = "quebec.culture.donnees"
  compileSdk = 35
- defaultConfig { applicationId = "quebec.culture.donnees"; minSdk = 26; targetSdk = 35; versionCode = 28; versionName = "3.6.1" }
+ defaultConfig { applicationId = "quebec.culture.donnees"; minSdk = 26; targetSdk = 35; versionCode = 29; versionName = "3.7.0" }
  buildTypes { release { isMinifyEnabled = false; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro") } }
  compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
 }
